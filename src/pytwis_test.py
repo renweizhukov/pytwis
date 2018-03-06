4f4dfee7cbe2d34d3c7903334467700b5f692f9f
Wei Ren <renwei2004@gmail.com>
2018-03-06 12:09:15 -0800

    Add a new test case for registering the same username in two threads simultaneously.
    Also create a test base class which has the general setUp() and tearDown() methods.
    
    Change-Id: I98a49a2b36c77039e88bdce8ce398672a3203cb2
    Signed-off-by: Wei Ren <renwei2004@gmail.com>

--- src/pytwis_test.py
+++ src/pytwis_test.py
@@ -7,11 +7,8 @@ from pytwis import Pytwis
 # By default the database index ranges from 0 to 15.
 TEST_DATABASE_ID = 15
 
-class PytwisRegisterTests(unittest.TestCase):
-    '''Test for the ``Pytwis.register()`` function.'''
-    
-    USERNAME = 'test_username'
-    PASSWORD = 'test_password'
+class PytwisTests(unittest.TestCase):
+    '''Pytwis test base class which has the general setUp() and tearDown() methods.'''
     
     def setUp(self):
         '''Set up the register test.
@@ -35,16 +32,41 @@ class PytwisRegisterTests(unittest.TestCase):
         Delete all the keys of the test database
         '''
         self._pytwis._rc.flushdb()
-        
-    def test_registerNewAndExistingUsers(self):
+
+class PytwisRegisterTests(PytwisTests):
+    '''Test for the ``Pytwis.register()`` function.'''
+    
+    def registerNewAndExistingUsers(self):
         '''Register a new user and then an existing user.'''
-        succeeded, _ = self._pytwis.register(self.USERNAME, self.PASSWORD)
+        username = 'test_username'
+        password = 'test_password'
+        succeeded, _ = self._pytwis.register(username, password)
         self.assertTrue(succeeded, 'Succeeded in registering a new username')
         
-        succeeded, result = self._pytwis.register(self.USERNAME, self.PASSWORD)
+        succeeded, result = self._pytwis.register(username, password)
         self.assertFalse(succeeded, 'Failed to register an existing username')
-        self.assertEqual(result['error'], 'username {} already exists'.format(self.USERNAME), 
+        self.assertEqual(result['error'], 'username {} already exists'.format(username), 
                          'Incorrect error message')
+        
+    def registerSameUserAtSameTime(self):
+        '''Register the same user in two threads almost simultaneously.'''
+        username = 'test2_username'
+        password = 'test2_password'
+        
+        from multiprocessing.pool import ThreadPool
+        pool = ThreadPool(processes=1)
+        
+        async_result1 = pool.apply_async(self._pytwis.register, (username, password))
+        async_result2 = pool.apply_async(self._pytwis.register, (username, password))
+        
+        succeeded1, _ = async_result1.get()
+        succeeded2, _ = async_result2.get()
+        
+        self.assertTrue(succeeded1 != succeeded2, 'One register should succeed and the other register should fail')
+        
+    def test_register(self):
+        self.registerNewAndExistingUsers()
+        self.registerSameUserAtSameTime()
         
 if __name__ == '__main__':
     unittest.main()
